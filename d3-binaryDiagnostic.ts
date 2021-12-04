22bbb1553108490da99a347ee69dab7462305e86
Jan Phillips <janpatekphillips@gmail.com>
2021-12-04 21:06:09 +0000

    day 3 part 1 complete

---
+++ d3-binaryDiagnostic.ts
@@ -0,0 +1,31 @@
+import * as fs from 'fs';
+
+const input = fs.readFileSync('day3-input.txt', 'utf-8');
+const data = input.replace(/(\r\n|\n|\r)/gm,"")
+let gamma: string = "";
+let epsilon: string = "";
+
+let columnSum: number[] = [0,0,0,0,0,0,0,0,0,0,0,0];
+
+let width: number = 12;
+let length: number = data.length
+
+for (let i = 0; i < width; i++) {
+    for (let j = i; j < length; j+= width) {
+        columnSum[i] += parseInt(data[j])
+    }
+}
+
+for (let k = 0; k < columnSum.length; k++) {
+    if (columnSum[k] > 500) {
+        gamma += "1"
+        epsilon += "0"
+    } else {
+        epsilon += "1"
+        gamma += "0"
+    }
+} 
+// answer
+let power: number = parseInt(gamma, 2) * parseInt(epsilon, 2)
+console.log(power)
+
